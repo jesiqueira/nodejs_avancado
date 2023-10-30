803d14da6a761637bc9fd26070f4af21625e01d2
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-10-30 07:51:20 -0300

    realiado configurações de Middlewares

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,13 @@
-import { Model, DataTypes } from 'sequelize'
+import { Model, DataTypes, Sequelize } from 'sequelize'
+import bcrypt from 'bcryptjs'
 
 class User extends Model {
     static init(sequelize) {
-        return super.init(
+        super.init(
             {
                 name: DataTypes.STRING,
                 email: DataTypes.STRING,
+                password: Sequelize.VIRTUAL,
                 password_hash: DataTypes.STRING,
             },
             {
@@ -17,6 +19,15 @@ class User extends Model {
                 },
             }
         )
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8)
+            }
+        })
+    }
+
+    checkPassword(password) {
+        return bcrypt.compare(password, this.password_hash)
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,39 @@
 import { Router } from 'express'
+
+import auth from './app/middlewares/auth'
+
+import session from './app/controllers/SessionsController'
 import customers from './app/controllers/CustomersController'
+import contacts from './app/controllers/ContactsController'
+import users from './app/controllers/UsersContoller'
 
 const routes = new Router()
 
+// Sessions
+routes.post('/sessions', session.create)
+
+// Controla o acesso a partir desse ponto
+routes.use(auth) // A partir daqui todas as rotas são protejidas por token
+
+// Customers
 routes.get('/customers', customers.index)
 routes.get('/customers/:id', customers.show)
 routes.post('/customers', customers.create)
 routes.put('/customers/:id', customers.update)
 routes.delete('/customers/:id', customers.destroy)
+
+// Contacts
+routes.get('/customers/:customerId/contacts', contacts.index)
+routes.get('/customers/:customerId/contacts/:id', contacts.show)
+routes.post('/customers/:customerId/contacts', contacts.create)
+routes.put('/customers/:customerId/contacts/:id', contacts.update)
+routes.delete('/customers/:customerId/contacts/:id', contacts.destroy)
+
+// Users
+routes.get('/users', users.index)
+routes.get('/users/:id', users.show)
+routes.post('/users', users.create)
+routes.put('/users/:id', users.update)
+routes.delete('/users/:id', users.destroy)
 
 export default routes
